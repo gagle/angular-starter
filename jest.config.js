c303b4c4135ec292b5652b121b190ef5ffd86c8b
Gabriel Llamas <gabriel.llamas@code.seat>
2020-09-02 16:46:14 +0200

    remove warning from jest execution

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,12 @@ module.exports = {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.html$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer'
-      ],
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer'
+        ]
+      },
       diagnostics: {
         ignoreCodes: [151001]
       },
