a26228444c36629a4c374b0b67599093f955697f
Gabriel Llamas <gabriel.llamas@code.seat>
2020-09-08 16:14:09 +0200

    chore: improve type-check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,4 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  constructor() {
-    const a = 34;
-  }
-}
+export class AppComponent {}
