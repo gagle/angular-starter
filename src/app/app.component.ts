44d883fec88f72dcc592884da59cefe5b4e64e37
Gabriel Llamas <gabriel.llamas@code.seat>
2020-09-16 15:20:00 +0200

    build: remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  val = of('asd');
-}
+export class AppComponent {}
